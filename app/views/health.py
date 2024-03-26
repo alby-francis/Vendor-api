884bae574df493b78a6762a9d9b2f4d4f15df263
Alby Francis <albyfrancis1@gmail.com>
2024-03-26 18:05:35 +0530

    add base route html

--- app/views/health.py
+++ app/views/health.py
@@ -1,8 +1,10 @@
 from rest_framework.response import Response
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
+from django.shortcuts import render
 
 @csrf_exempt
 @api_view(['GET'])
 def check_health(request):
-    return Response([])
+    return render(request, 'health.html')
+    #return Response({"message": "Server is up"})
